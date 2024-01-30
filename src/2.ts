c348deff60262ccd0d56e9b82744985a42bfb770
Tania <113130993+Tania66@users.noreply.github.com>
2024-01-30 13:09:07 +0200

    hw

--- src/2.ts
+++ src/2.ts
@@ -15,10 +15,10 @@
 */
 
 class Employee {
-  // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
+
+  public name: string;
+ private  department: string;
+ protected salary: number;
 
   constructor(name: string, department: string, salary: number) {
     this.name = name;
@@ -32,8 +32,18 @@ class Employee {
 }
 
 class Manager extends Employee {
-  // Реалізуйте конструктор та збільшіть salary на 10000
+
+  constructor(name:string, salary:number){
+     super(name, "TypeScript", salary +10000) 
+  }
+
+  details() {
+    return `${super.getEmployeeDetails()} `;
 }
+}
+
+const manager = new Manager("bob", 10000);
+console.log(manager.details());
 
 
 export {};
--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,49 @@
+class Key {
+
+    private signature: number = Math.floor(10 * Math.random());
+
+    getSignature(): number {
+        return this.signature;
+    }
+}
+
+
+class Person {
+    constructor(private key: Key) { }
+
+    getKey() {
+        return this.key;
+    }
+}
+
+
+abstract class House {
+    protected door: boolean = false;
+    private tenants: Person[] = [];
+
+    constructor(protected key: Key) { }
+
+    comeIn(tenant: Person) {
+        if (this.door === true) {
+            this.tenants.push(tenant);
+        }
+    }
+
+    abstract openDoor(key: Key): void;
+}
+
+
+
+class MyHouse extends House {
+    public openDoor(key: Key): void {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door === true;
+        }
+        return;
+    }
+}
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +52,6 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
+export { };
 
 
-export {};
